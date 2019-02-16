d827e1708eaa7e4f643a02e61ac41a02b919ca1a
Parth Bhatia <parthbhatia98@gmail.com>
2019-02-16 21:19:57 +0530

    X01BD: Inherit perf blobs from vendor/qcom/common
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Mihran Thalhath <mihranz7@gmail.com>
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -298,6 +298,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# Performance
+-include vendor/qcom/common/qti-vendor.mk
+
 # Powerhint
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
