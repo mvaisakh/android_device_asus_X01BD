2efd3d09ae76038ad8829105c028aef8f0ae79f6
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-05-28 11:31:34 +0600

    X01BD: Use QTI Vibrator HAL
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -141,8 +141,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator@1.2-service
 
 # VR
 PRODUCT_PACKAGES += \
